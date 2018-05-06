a720189c015195e9d48cc2321bfde66dc8733f40
Yoshihiro Tanaka <contact@cordea.jp>
2018-05-06 18:48:36 +0900

    fix the append api

--- lib/scripts.dart
+++ lib/scripts.dart
@@ -2,41 +2,45 @@ import 'dart:async';
 
 import 'package:googleapis/script/v1.dart';
 import 'package:http/http.dart' as http;
-import 'package:laxly_account/row.dart';
 import 'package:laxly_account/app_key.dart';
+import 'package:laxly_account/row.dart';
 
 class Scripts {
 
   ScriptApi _api;
-  final String _id;
+  final String _sheetId;
 
-  Scripts(http.Client client, this._id) {
+  Scripts(http.Client client, this._sheetId) {
     _api = new ScriptApi(client);
   }
 
   Future<Operation> _run(String name, List<Object> data) {
     var request = ExecutionRequest();
-    request.devMode = false;
+    request.devMode = true;
     request.function = name;
     request.parameters = [
-      AppKey.sheet_id,
+      _sheetId,
       data
     ];
 
-    return _api.scripts.run(request, _id);
+    return _api.scripts.run(request, AppKey.SCRIPT_ID);
   }
 
-  Future<List<Row>> get() {
+  Future<List<SheetRow>> get() {
     return _run("get", []).then((operation) {
       return (operation.response["result"] as List)
           .map((item) {
         var list = item as List;
-        return Row(list[0], list[1], list[2]);
+        return SheetRow(list[0], list[1], list[2]);
       }).toList();
     });
   }
 
-  Future append(int price) {
-    return _run("append", [price]);
+  Future append(SheetRow row) {
+    return _run("append", [
+      row.year,
+      row.month,
+      row.price
+    ]);
   }
 }
